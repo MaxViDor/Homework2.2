07339ec59c1e10593e9e9c873b49f0669d609d61
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-19 01:35:58 +0300

    add first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     task1 ();
+     task2 ();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte b = 1;
+        System.out.println("Значение переменной b с типом byte равно " +b);
+        short s = 12;
+        System.out.println("Значение переменной s с типом short равно " +s);
+        int i = 123;
+        System.out.println("Значение переменной i с типом int равно " +i);
+        long l = 1234L;
+        System.out.println("Значение переменной l с типом long равно " +l);
+        float f = 1.1f;
+        System.out.println("Значение переменной f с типом float равно " +f);
+        double d = 1.12;
+        System.out.println("Значение переменной d с типом double равно " +d);
+
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
     }
 }
